297c0883dcf57e59e47ceaacd729a59ec38bdf20
timoCS90 <timo.dworog@gmail.com>
2024-07-20 14:34:59 +0200

    fixed issues

--- model/Book.js
+++ model/Book.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
-const blogSchema = new Schema({
+const bookSchema = new Schema({
   title: { type: String, required: true },
   summary: { type: String, required: true },
   isbn: { type: String, required: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author', required: true }
-  };
+});
   
 
-const Book = model('Book', blogSchema);
+const Book = model('Book', bookSchema);
 export default Book;
